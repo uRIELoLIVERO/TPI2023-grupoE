98dc2c2dd5b040d5a5e0ca67a347680463c10fb2
Facundo Anil <facucsgo647@gmail.com>
2023-11-13 16:30:46 -1200

    Lista productos

--- PaginaCantina/mysite/settings.py
+++ PaginaCantina/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,9 @@ SECRET_KEY = 'django-insecure-=@8n)6n+^48m=l5pzln#qe7m!gx7z595^!f##ozd$9#p3u3-1k
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Application definition
