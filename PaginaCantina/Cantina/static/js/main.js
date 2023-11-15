c4f4fe04beab72dd8c065bb37f4643c8160ae212
uRIELoLIVERO <oliverouri4@gmail.com>
2023-11-15 19:08:45 -0300

    navbar con algunos links, template productos funcionando, estilos al login

--- PaginaCantina/Cantina/static/js/main.js
+++ PaginaCantina/Cantina/static/js/main.js
@@ -1,9 +1,19 @@
 const menuNavbar = document.querySelector('#burgerMenu')
 const menuInfo = document.querySelector('#infoMenu')
+const buttonFilter = document.querySelector('#filter-options')
+const menuFilter = document.querySelector('#filter-form')
+const containerFind = document.querySelector('#containerFind')
 
 menuNavbar.addEventListener('click', toggleMenu)
+buttonFilter.addEventListener('click', toggleFilter)
 
 function toggleMenu (){
     menuInfo.classList.toggle('inactive')
     menuNavbar.classList.toggle('superponer')
+}
+function toggleFilter(){
+    menuFilter.classList.toggle('inactive')
+    console.log('hola')
+    containerFind.classList.toggle('margin-largo')
+    containerFind.classList.toggle('margin-corto')
 }
